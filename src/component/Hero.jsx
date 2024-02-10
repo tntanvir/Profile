3bdec65d0fb8db0e4ac9dde7d0f6684b94f898b5
Tanvir <tntanvir2382018@gmail.com>
2024-02-10 19:47:25 +0600

    hero ui change

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -2,11 +2,18 @@ import { Button } from '@material-tailwind/react';
 import React from 'react';
 import { TypeAnimation } from 'react-type-animation';
 import { FaFacebook, FaInstagram, FaGithub } from "react-icons/fa";
+import { useContext } from 'react';
+import { gitUser } from '../App';
 const Hero = () => {
+    const [gThem, setGThem] = useContext(gitUser);
     return (
         <div className='h-[88vh] hero flex justify-center flex-col lg:px-28 md:gap-7 gap-2 px-2 items-center relative'>
+            {gThem === "light" && <img src="/src/assets/1.png" alt="" className='w-24 absolute top-10 left-28  hidden md:block' />}
+            {gThem === "light" && <img src="/src/assets/5.png" alt="" className='w-24 absolute top-10 right-28 hidden md:block' />}
+            {gThem === "light" && <img src="/src/assets/3.png" alt="" className='w-24 absolute left-48 bottom-28 hidden md:block' />}
+            {gThem === "light" && <img src="/src/assets/4.png" alt="" className='w-28 absolute right-48 bottom-24 hidden md:block ' />}
             <h1 className='text-2xl'>Hello <span className='text-dkText mr-3'>!</span>   i'm</h1>
-            <h1 className='md:text-7xl text-3xl font-bold tracking-[0.3rem] '>Tanvir Rahman</h1>
+            <h1 className='md:text-7xl text-3xl font-bold tracking-[0.3rem] z-10'>Tanvir Rahman</h1>
             <div className='text-dkText'>
                 <TypeAnimation
                     sequence={[
